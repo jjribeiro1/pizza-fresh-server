37f83b7a48bda0c3950d41ec69bbcc106ea23aef
jjribeiro1 <jvsr.99@hotmail.com>
2022-10-11 19:50:52 -0300

    tipagem nos endpoints e implementação findone table

--- src/table/table.controller.ts
+++ src/table/table.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { CreateTableDto } from './create-table-dto';
+import { Table } from './entities/table.entity';
 import { TableService } from './table.service';
 
 @ApiTags('table')
@@ -9,12 +10,17 @@ export class TableController {
   constructor(private readonly tableService: TableService) {}
 
   @Get()
-  findAll() {
+  findAll(): Promise<Table[]> {
     return this.tableService.findAll();
   }
 
+  @Get('/:id')
+  findOne(@Param('id') id: string): Promise<Table> {
+    return this.tableService.findOne(id);
+  }
+
   @Post()
-  create(@Body() dto: CreateTableDto) {
+  create(@Body() dto: CreateTableDto): Promise<Table> {
     return this.tableService.create(dto);
   }
 }
