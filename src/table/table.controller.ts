41b07fbdad0d7298ae5f617bebc59837ec1ce5ff
jjribeiro1 <jvsr.99@hotmail.com>
2022-10-11 19:55:24 -0300

    "sumário swagger docs"

--- src/table/table.controller.ts
+++ src/table/table.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateTableDto } from './create-table-dto';
 import { Table } from './entities/table.entity';
 import { TableService } from './table.service';
@@ -10,16 +10,25 @@ export class TableController {
   constructor(private readonly tableService: TableService) {}
 
   @Get()
+  @ApiOperation({
+    summary: 'Listar todas as mesas',
+  })
   findAll(): Promise<Table[]> {
     return this.tableService.findAll();
   }
 
   @Get('/:id')
+  @ApiOperation({
+    summary: 'Buscar uma única mesa',
+  })
   findOne(@Param('id') id: string): Promise<Table> {
     return this.tableService.findOne(id);
   }
 
   @Post()
+  @ApiOperation({
+    summary: 'Criar uma mesa',
+  })
   create(@Body() dto: CreateTableDto): Promise<Table> {
     return this.tableService.create(dto);
   }
