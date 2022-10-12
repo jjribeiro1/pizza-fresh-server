69b534b3a0b322f247bf1e137ec580fad4ed4f15
jjribeiro1 <jvsr.99@hotmail.com>
2022-10-12 14:38:15 -0300

    "reestrurando arquivos app"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TableModule } from './table/table.module';
-import { PrismaModule } from './prisma/prisma.module';
+import { TableModule } from '../table/table.module';
+import { ProductModule } from '../product/product.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [TableModule, PrismaModule],
+  imports: [TableModule, ProductModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
