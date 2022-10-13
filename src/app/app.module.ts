a51f6d42c7bcc8c6ca37180c4c16162630939232
jjribeiro1 <jvsr.99@hotmail.com>
2022-10-12 23:56:19 -0300

    "fix importando userModule no app"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { TableModule } from '../table/table.module';
 import { ProductModule } from '../product/product.module';
 import { PrismaModule } from '../prisma/prisma.module';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [TableModule, ProductModule, PrismaModule],
+  imports: [TableModule, ProductModule, UserModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
