90dac9b416a0cf7eb41444a29efb34df146881ff
jjribeiro1 <jvsr.99@hotmail.com>
2022-10-12 23:34:36 -0300

    "criação do dto e user entity"

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,42 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsUrl, Matches, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @ApiProperty({
+    description: 'Nome de usuário utilizado para login, deve ser único',
+    example: 'username123',
+  })
+  nickname: string;
+
+  @IsString()
+  @MinLength(6)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Senha muito fraca',
+  })
+  @ApiProperty({
+    description: 'Senha de usuário para login',
+    example: 'Abcd123@',
+  })
+  password: string;
+
+  @ApiProperty({
+    description: 'A confirmação da senha deve ser igual a senha',
+    example: 'Abcd@123@',
+  })
+  confirmPassword: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'Nome próprio do usuário, apenas para exibição',
+    example: 'João Vitor',
+  })
+  name: string;
+
+  @IsUrl()
+  @ApiProperty({
+    description: 'Foto do usuário',
+    example: 'https://avatars.githubusercontent.com/u/103435236?v=4',
+  })
+  image: string;
+}
