26338bdc1b4f07b4fc46f39981d2f1b7509e1ebb
jjribeiro1 <jvsr.99@hotmail.com>
2022-10-14 15:09:54 -0300

    "order swagger docs"

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { OrderService } from './order.service';
 
@@ -9,16 +9,25 @@ export class OrderController {
   constructor(private readonly orderService: OrderService) {}
 
   @Post()
+  @ApiOperation({
+    summary: 'Criar um pedido',
+  })
   create(@Body() dto: CreateOrderDto) {
     return this.orderService.create(dto);
   }
 
   @Get()
+  @ApiOperation({
+    summary: 'Listar todos os pedido',
+  })
   findAll() {
     return this.orderService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: 'Buscar um pedido por id',
+  })
   findOne(@Param('id') id: string) {
     return this.orderService.findOne(id);
   }
